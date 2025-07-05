a330e0d890677a781591727ca696e580eb3f9159
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-05 13:51:30 +0530

    Add Student Controller

---
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/controller/StudentController.java
@@ -0,0 +1,17 @@
+package lk.ijse.cmjd_110.courseRegisterPro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/student/")
+public class StudentController {
+     //Todo: Handle Student's details as CRUD operation
+
+    //Save Student
+
+
+
+
+}
