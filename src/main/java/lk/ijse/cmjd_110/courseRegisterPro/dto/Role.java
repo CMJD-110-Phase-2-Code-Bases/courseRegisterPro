1a7d3aca3e09579ee68b060bd16807c4c28e497c
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-05 14:09:20 +0530

    Create UserDTO

---
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/dto/Role.java
@@ -0,0 +1,5 @@
+package lk.ijse.cmjd_110.courseRegisterPro.dto;
+
+public enum Role {
+    ADMIN,STUDENT,LECTURER
+}
---
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package lk.ijse.cmjd_110.courseRegisterPro.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO implements Serializable {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String city;
+    private String password;
+    private Role role;
+}
