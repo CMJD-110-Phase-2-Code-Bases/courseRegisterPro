d42a7e011a743fb19ef4624dd6dd259b4d12aa7e
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-17 21:59:43 +0530

    Handle a Multipart file

--- src/main/java/lk/ijse/cmjd_110/courseRegisterPro/controller/CourseMaterialController.java
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/controller/CourseMaterialController.java
@@ -6,12 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 
 @RestController
 @RequestMapping("api/v1/material")
 public class CourseMaterialController {
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CourseMaterialDTO> saveMaterial(
             @RequestParam("materialIdentity") String materialId,
             @RequestParam String fileName,
@@ -29,14 +31,20 @@ public class CourseMaterialController {
             byte [] materialBytes = material.getBytes();
             //Step 2.2: Develop a Base64 String based on the byte collection
             String base64Material = Base64.getEncoder().encodeToString(materialBytes);
+        //Step-3: Handle upload time
+            String uploadTime = uploadAt != null ? uploadAt : LocalDate.now().format(DateTimeFormatter.ISO_DATE);
 
-
+        //Step-4: Build the entire object
+            courseMaterial.setCourseId(materialId);
+            courseMaterial.setMaterialType(materialType);
+            courseMaterial.setFileName(fileName);
+            courseMaterial.setMaterial(base64Material);
+            courseMaterial.setUploadAt(uploadTime);
+            courseMaterial.setCourseId(courseId);
         }catch (Exception ex){
             ex.printStackTrace();
         }
-
-
-
+        return ResponseEntity.ok(courseMaterial);
 
     }
 }
