ea8211a713b6a8a14346b5e5055cb6a0e722ab78
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-19 14:25:48 +0530

    Create entities

---
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/entities/CourseEntity.java
@@ -0,0 +1,29 @@
+package lk.ijse.cmjd_110.courseRegisterPro.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class CourseEntity {
+    @Id
+    private String courseId;
+    private String courseCode;
+    private String courseName;
+    private String description;
+    private Integer credit;
+    private String dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    @ManyToOne
+    @JoinColumn(name = "lecId")
+    private LecturerEntity instructorId;
+}
---
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/entities/CourseMaterialEntity.java
@@ -0,0 +1,27 @@
+package lk.ijse.cmjd_110.courseRegisterPro.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class CourseMaterialEntity {
+    @Id
+    private String materialId;
+    private String fileName;
+    private String materialType;
+    private String material;
+    private LocalDateTime uploadAt;
+    @ManyToOne
+    @JoinColumn(name = "courseId")
+    private CourseEntity courseId;
+}
