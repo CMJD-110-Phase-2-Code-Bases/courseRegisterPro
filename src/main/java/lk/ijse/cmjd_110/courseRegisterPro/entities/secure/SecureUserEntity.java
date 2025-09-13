4495919ef65c5deb06b5781632c3a96d01c97bb4
ThanuraSilva <thanurasilva1989@gmail.com>
2025-09-13 13:18:15 +0530

    Create SecureUserEntity

---
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/entities/secure/SecureUserEntity.java
@@ -0,0 +1,43 @@
+package lk.ijse.cmjd_110.courseRegisterPro.entities.secure;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jdk.jfr.Enabled;
+import lk.ijse.cmjd_110.courseRegisterPro.dto.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class SecureUserEntity implements Serializable, UserDetails {
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
