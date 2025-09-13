47b00a3ed8b45d5b13ff31678daa1d09841b9f82
ThanuraSilva <thanurasilva1989@gmail.com>
2025-09-13 13:29:31 +0530

    Add AuthEntryPoint and AuthFilter (add only)

---
+++ src/main/java/lk/ijse/cmjd_110/courseRegisterPro/secureConfig/AuthFilter.java
@@ -0,0 +1,25 @@
+package lk.ijse.cmjd_110.courseRegisterPro.secureConfig;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Configuration
+@RequiredArgsConstructor
+@Order(1)
+public class AuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+       //Todo: IMPL
+    }
+}
